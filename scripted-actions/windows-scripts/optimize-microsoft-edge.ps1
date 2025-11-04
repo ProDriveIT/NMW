cbed164d2a438cf67d942c3cf842d1077ae91f28
ProDriveIT <support@prodriveit.co.uk>
2025-11-04 16:13:08 +0000

    Update optimize-microsoft-edge.ps1

--- scripted-actions/windows-scripts/optimize-microsoft-edge.ps1
+++ scripted-actions/windows-scripts/optimize-microsoft-edge.ps1
@@ -2,12 +2,22 @@
 #tags: Nerdio, Preview
 <#
 Notes:
-This script configures policy settings for Microsoft Edge meant to optimize performance.
+This script configures policy settings for Microsoft Edge meant to optimize performance in AVD environments.
 Policies Set: 
-    - Enable Sleeping Tabs ("sleep" inactive browser tabs) 
-    - Enable Startup Boost (preload MS Edge in the background on login)
+    - Enable Sleeping Tabs ("sleep" inactive browser tabs) - Reduces memory usage for inactive tabs
+    - Disable Startup Boost - Prevents Edge from preloading at login, reducing resource usage
+    - Disable Background Mode - Prevents Edge from running in background when closed
+    - Enable Efficiency Mode - Reduces resource consumption in VDI environments
+    - Hide First Run Experience - Improves login performance by skipping initial setup
+    - Disable Recommendations - Reduces background activity and resource usage
+    - Disable Web Widget - Reduces background activity
 #>
 
-# Set registry settings
+# Set registry settings for AVD optimization
 reg add HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Edge /v "SleepingTabsEnabled" /t REG_DWORD /d 1 /f
-reg add HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Edge /v "StartupBoostEnabled" /t REG_DWORD /d 1 /f
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Edge /v "StartupBoostEnabled" /t REG_DWORD /d 0 /f
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Edge /v "BackgroundModeEnabled" /t REG_DWORD /d 0 /f
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Edge /v "EfficiencyMode" /t REG_DWORD /d 1 /f
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Edge /v "HideFirstRunExperience" /t REG_DWORD /d 1 /f
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Edge /v "ShowRecommendationsEnabled" /t REG_DWORD /d 0 /f
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Edge /v "WebWidgetAllowed" /t REG_DWORD /d 0 /f
