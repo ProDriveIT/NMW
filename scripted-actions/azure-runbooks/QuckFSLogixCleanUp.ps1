cae78b318b29474ebcbf7f240d758ee51b9820ad
ProDriveIT <support@prodriveit.co.uk>
2025-11-07 15:56:18 +0000

    Create QuckFSLogixCleanUp.ps1

---
+++ scripted-actions/azure-runbooks/QuckFSLogixCleanUp.ps1
@@ -0,0 +1,12 @@
+﻿
+# Set parameters
+$StorageAccountName = "scstfslavdproduks001"
+$ShareName = "avd-profiles"
+$DaysOld = 90
+$StorageKeySecureVar = "vgIB77llF0JT3EkGdiDku4rgIZDFzb52GHtfplztouB+HNiK9++bKaUzsF5qFzetT1nuH+Cm+Ik1+AStqgPtRA=="
+$WhatIf = "true"  # Use "true" to test, "false" to actually delete
+
+# Run the script
+cd "C:\Dev\NMW\scripted-actions\azure-runbooks"
+
+.\DeleteOldFSLogixProfiles.ps1
